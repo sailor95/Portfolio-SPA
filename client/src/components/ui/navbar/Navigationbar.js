918a23909986f13a0d3094478edae3e4e3fe53be
David Huang <bugman195@gmail.com>
2019-04-20 00:36:48 +0800

    [client] Add Axure to navbar

--- client/src/components/ui/navbar/Navigationbar.js
+++ client/src/components/ui/navbar/Navigationbar.js
@@ -6,15 +6,30 @@ import classes from './Navigationbar.module.css';
 
 const Navigationbar = () => {
   return (
-    <div>
-      <Navbar bg="dark">
+    <div className={`contaner-fluid ${classes.NavBackground}`}>
+      <nav className="navbar navbar-expand-lg">
         <Navbar.Brand>
           <Link to="/" className={classes.Brand}>
             David Huang
           </Link>
         </Navbar.Brand>
-      </Navbar>
-    </div>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarNavDropdown" className="navbar-collapse collapse">
+          <ul className="navbar-nav mr-auto">
+            {/* <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li> */}
+          </ul>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" target="_blank" rel="noopener noreferrer" href="https://2l6nt4.axshare.com">Axure Prototype</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div >
   );
 }
 
