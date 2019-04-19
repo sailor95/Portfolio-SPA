3fb34c9eb1d6808355aed168d4826e5aac12699f
David Huang <david.huang@getac.com.tw>
2019-04-19 15:13:38 +0800

    [client] Add links for sidebar

--- client/src/containers/home/home-sidebar/HomeSidebar.js
+++ client/src/containers/home/home-sidebar/HomeSidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 import classes from './HomeSidebar.module.css';
 
@@ -7,10 +8,10 @@ const HomeSidebar = () => {
   return (
     <Col>
       <Row className={classes.SideBarTitle}>My Works</Row>
-      <Row className={classes.RowItem}><button className={classes.BigButton}>Software Projects</button></Row>
-      <Row className={classes.RowItem}><button className={classes.BigButton}>Podcast</button></Row>
-      <Row className={classes.RowItem}><button className={classes.BigButton}>Articles</button></Row>
-      <Row className={classes.RowItem}><button className={classes.BigButton}>Photography / Design</button></Row>
+      <Row className={classes.RowItem}><Link to="/sw-projects"><button className={classes.BigButton}>Software Projects</button></Link></Row>
+      <Row className={classes.RowItem}><Link to="/podcasts"><button className={classes.BigButton}>Podcast</button></Link></Row>
+      <Row className={classes.RowItem}><Link to="/articles"><button className={classes.BigButton}>Articles</button></Link></Row>
+      <Row className={classes.RowItem}><Link to="/photos-design"><button className={classes.BigButton}>Photography / Design</button></Link></Row>
     </Col>
   );
 }
