d25debd12e6af5076b173b76a5beef7e2ff1d025
David Huang <david.huang@getac.com.tw>
2019-04-22 14:44:27 +0800

    [client] Format JSX

--- client/src/containers/home/home-sidebar/HomeSidebar.js
+++ client/src/containers/home/home-sidebar/HomeSidebar.js
@@ -7,11 +7,20 @@ import classes from './HomeSidebar.module.css';
 const HomeSidebar = () => {
   return (
     <Col>
-      <Row className={classes.SideBarTitle}>My Works</Row>
-      <Row className={classes.RowItem}><Link to="/sw-projects"><button className={classes.BigButton}>Software Projects</button></Link></Row>
-      <Row className={classes.RowItem}><Link to="/podcasts"><button className={classes.BigButton}>Podcast</button></Link></Row>
-      <Row className={classes.RowItem}><Link to="/articles"><button className={classes.BigButton}>Articles</button></Link></Row>
-      <Row className={classes.RowItem}><Link to="/photos-design"><button className={classes.BigButton}>Photography / Design</button></Link></Row>
+      <Row className={classes.SideBarTitle}>
+        My Works
+      </Row>
+      <Row className={classes.RowItem}>
+        <Link to="/sw-projects"><button className={classes.BigButton}>Software Projects</button></Link>
+      </Row>
+      <Row className={classes.RowItem}>
+        <Link to="/podcasts"><button className={classes.BigButton}>Podcast</button></Link></Row>
+      <Row className={classes.RowItem}>
+        <Link to="/articles"><button className={classes.BigButton}>Articles</button></Link>
+      </Row>
+      <Row className={classes.RowItem}>
+        <Link to="/photos-design"><button className={classes.BigButton}>Photography / Design</button></Link>
+      </Row>
     </Col>
   );
 }
