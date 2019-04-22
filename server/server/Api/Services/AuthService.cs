9c824d7b91a2ffa970354fdebec6755c2eed29ba
David Huang <bugman195@gmail.com>
2019-04-22 23:44:08 +0800

    [server] Clean code

--- server/server/Api/Services/AuthService.cs
+++ server/server/Api/Services/AuthService.cs
@@ -3,12 +3,9 @@ using Microsoft.IdentityModel.Tokens;
 using server.Api.Services.Abstraction;
 using server.Api.ViewModels;
 using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace server.Api.Services
 {
