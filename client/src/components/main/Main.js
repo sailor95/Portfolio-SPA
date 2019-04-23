3a536a1d6fae2d90bb036a83d1fb96dccf0e27dd
David Huang <bugman195@gmail.com>
2019-04-23 23:21:36 +0800

    [client] Add Dashboard as PrivateRoute

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+// Components
 import Navigationbar from '../presentational/ui/navbar/Navigationbar';
 import Home from '../../containers/home/Home';
 import SoftwareProjectsPage from '../../containers/software-projects-page/SoftwareProjectsPage';
@@ -9,6 +10,8 @@ import ArticlesPage from '../../containers/articles-page/ArticlesPage';
 import PhotosAndDesignPage from '../../containers/photo-and-design-page/PhotosAndDesignPage';
 import Login from '../presentational/login/Login';
 import Register from '../presentational/register/Register';
+import { PrivateRoute } from '../functional/privateRoute/PrivateRoute';
+import Dashboard from '../../containers/dashboard/Dashboard';
 
 
 const main = () => {
@@ -18,6 +21,7 @@ const main = () => {
         <Navigationbar />
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/sw-projects" component={SoftwareProjectsPage} />
         <Route exact path="/podcasts" component={PodcastsPage} />
