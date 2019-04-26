fa4ce29d0ba3f61e030d17a7ce31845070f9e4a3
David Huang <david.huang@getac.com.tw>
2019-04-26 16:22:41 +0800

    [client] Api setting

--- client/src/api/spa-server/spaServer.js
+++ client/src/api/spa-server/spaServer.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  //baseURL: 'https://portfolio-webapi.azurewebsites.net/api/'
-  baseURL: 'https://localhost:44318/api/'
+  baseURL: 'https://portfolio-webapi.azurewebsites.net/api/'
+  //baseURL: 'https://localhost:44318/api/'
 });
